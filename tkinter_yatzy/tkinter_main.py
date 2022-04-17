4c151cd1f08bd1b2bbf80993adfc9f4496207fb0
Karolina Mäkinen <karolina.makinen1@gmail.com>
2022-04-17 19:47:43 +0300

    Added functions

--- tkinter_yatzy/tkinter_main.py
+++ tkinter_yatzy/tkinter_main.py
@@ -4,12 +4,11 @@
 
 # Import classes
 
+from setuptools import Command
 from tkinter_roll import Roll # Roll
 from tkinter_player import Player # Player
 from tkinter import *
 import random
-
-
 
 def game(player_list):
 
@@ -63,18 +62,16 @@ def game(player_list):
     btn = Button(root, text="Click for rules",width= 20, height = 2, bg='lightyellow', command = open_new_window)
     btn.grid(row= 21,rowspan=2, column=2, columnspan=2)
 
-
     t2 = Label(root, text = '', height= 4, width= 45, borderwidth=2, relief='groove')
     t2.grid(row=7, rowspan=4, column=10, columnspan=5, sticky='e')
-
-
+    
     for i in range(15):
-
+        
         for player in player_list:
-
-            btn.configure(state='normal')
+                
+            btn.configure(state='active')
             t2.configure(text=player.get_name() +", your turn")
-
+            
             def place_ones():
                 ones = dice.check_ones(dice_numbers)
                 cell1 = Label(root, text= ones, bg='white', width=10, borderwidth=1, relief='raised')
@@ -84,27 +81,180 @@ def game(player_list):
                 t2.configure(text=player.get_name() +", your turn")
                 for die in dice_list:
                     die.configure(bg='lightgrey')
-                root.counter = 0
+                    root.counter = 0
                 
+            def place_twos():
+                twos = dice.check_twos(dice_numbers)
+                cell2 = Label(root, text= twos, bg='white', width=10, borderwidth=1, relief='raised')
+                cell2.grid(row=2, column=i+1)
+                player.add_top_score(twos)
+                btn.configure(state='normal')
+                t2.configure(text=player.get_name() +", your turn")
+                for die in dice_list:
+                    die.configure(bg='lightgrey')
+                    root.counter = 0
+
+            def place_threes():
+                threes = dice.check_threes(dice_numbers)
+                cell3 = Label(root, text= threes, bg='white', width=10, borderwidth=1, relief='raised')
+                cell3.grid(row=3, column=i+1)
+                player.add_top_score(threes)
+                btn.configure(state='normal')
+                t2.configure(text=player.get_name() +", your turn")
+                for die in dice_list:
+                    die.configure(bg='lightgrey')
+                    root.counter = 0
+            
+            def place_fours():
+                fours = dice.check_fours(dice_numbers)
+                cell4 = Label(root, text= fours, bg='white', width=10, borderwidth=1, relief='raised')
+                cell4.grid(row=4, column=i+1)
+                player.add_top_score(fours)
+                btn.configure(state='normal')
+                t2.configure(text=player.get_name() +", your turn")
+                for die in dice_list:
+                    die.configure(bg='lightgrey')
+                    root.counter = 0
+
+            def place_fives():
+                fives = dice.check_fives(dice_numbers)
+                cell5 = Label(root, text= fives, bg='white', width=10, borderwidth=1, relief='raised')
+                cell5.grid(row=5, column=i+1)
+                player.add_top_score(fives)
+                btn.configure(state='normal')
+                t2.configure(text=player.get_name() +", your turn")
+                for die in dice_list:
+                    die.configure(bg='lightgrey')
+                    root.counter = 0
+
+            def place_sixes():
+                sixes = dice.check_sixes(dice_numbers)
+                cell6 = Label(root, text= sixes, bg='white', width=10, borderwidth=1, relief='raised')
+                cell6.grid(row=6, column=i+1)
+                player.add_top_score(sixes)
+                btn.configure(state='normal')
+                t2.configure(text=player.get_name() +", your turn")
+                for die in dice_list:
+                    die.configure(bg='lightgrey')
+                    root.counter = 0
+
+            def place_one_pair():
+                one_pair = dice.check_one_pair(dice_numbers)
+                cell9 = Label(root, text= one_pair, bg='white', width=10, borderwidth=1, relief='raised')
+                cell9.grid(row=9, column=i+1)
+                player.add_bottom_score(one_pair)
+                btn.configure(state='normal')
+                t2.configure(text=player.get_name() +", your turn")
+                for die in dice_list:
+                    die.configure(bg='lightgrey')
+                    root.counter = 0
+
+            def place_two_pair():
+                two_pair = dice.check_two_pairs(dice_numbers)
+                cell10 = Label(root, text= two_pair, bg='white', width=10, borderwidth=1, relief='raised')
+                cell10.grid(row=10, column=i+1)
+                player.add_bottom_score(two_pair)
+                btn.configure(state='normal')
+                t2.configure(text=player.get_name() +", your turn")
+                for die in dice_list:
+                    die.configure(bg='lightgrey')
+                    root.counter = 0
+
+            def place_three_of_kind():
+                three_kind = dice.check_three_kind(dice_numbers)
+                cell11 = Label(root, text= three_kind, bg='white', width=10, borderwidth=1, relief='raised')
+                cell11.grid(row=11, column=i+1)
+                player.add_bottom_score(three_kind)
+                btn.configure(state='normal')
+                t2.configure(text=player.get_name() +", your turn")
+                for die in dice_list:
+                    die.configure(bg='lightgrey')
+                    root.counter = 0
+            
+            def place_four_of_kind():
+                four_kind = dice.check_four_kind(dice_numbers)
+                cell2 = Label(root, text= four_kind, bg='white', width=10, borderwidth=1, relief='raised')
+                cell2.grid(row=12, column=i+1)
+                player.add_bottom_score(four_kind)
+                btn.configure(state='normal')
+                t2.configure(text=player.get_name() +", your turn")
+                for die in dice_list:
+                    die.configure(bg='lightgrey')
+                    root.counter = 0
+            
+            def place_full_house():
+                full_house = dice.check_full_house(dice_numbers)
+                cell13 = Label(root, text= full_house, bg='white', width=10, borderwidth=1, relief='raised')
+                cell13.grid(row=13, column=i+1)
+                player.add_bottom_score(full_house)
+                btn.configure(state='normal')
+                t2.configure(text=player.get_name() +", your turn")
+                for die in dice_list:
+                    die.configure(bg='lightgrey')
+                    root.counter = 0
+
+            def place_low_straight():
+                low_straight = dice.check_low_straight(dice_numbers)
+                cell14 = Label(root, text= low_straight, bg='white', width=10, borderwidth=1, relief='raised')
+                cell14.grid(row=14, column=i+1)
+                player.add_bottom_score(low_straight)
+                btn.configure(state='normal')
+                t2.configure(text=player.get_name() +", your turn")
+                for die in dice_list:
+                    die.configure(bg='lightgrey')
+                    root.counter = 0
+
+            def place_high_straight():
+                high_straight = dice.check_high_straight(dice_numbers)
+                cell5 = Label(root, text= high_straight, bg='white', width=10, borderwidth=1, relief='raised')
+                cell5.grid(row=15, column=i+1)
+                player.add_bottom_score(high_straight)
+                btn.configure(state='normal')
+                t2.configure(text=player.get_name() +", your turn")
+                for die in dice_list:
+                    die.configure(bg='lightgrey')
+                    root.counter = 0
+
+            def place_chance():
+                chance = dice.check_chance(dice_numbers)
+                cell6 = Label(root, text= chance, bg='white', width=10, borderwidth=1, relief='raised')
+                cell6.grid(row=16, column=i+1)
+                player.add_bottom_score(chance)
+                btn.configure(state='normal')
+                t2.configure(text=player.get_name() +", your turn")
+                for die in dice_list:
+                    die.configure(bg='lightgrey')
+                    root.counter = 0
+
+            def place_yatzy():
+                yatzy = dice.check_yatzy(dice_numbers)
+                cell7 = Label(root, text= yatzy, bg='white', width=10, borderwidth=1, relief='raised')
+                cell7.grid(row=17, column=i+1)
+                player.add_bottom_score(yatzy)
+                btn.configure(state='normal')
+                t2.configure(text=player.get_name() +", your turn")
+                for die in dice_list:
+                    die.configure(bg='lightgrey')
+                    root.counter = 0
 
             # creating scoreboard buttons and labels
-            Button(root, text='Ones', command= place_ones).grid(row=1, column=0, sticky='ew')
-            Button(root, text='Twos').grid(row=2, column=0, sticky='ew')
-            Button(root, text='Threes').grid(row=3, column=0, sticky='ew')
-            Button(root, text='Fours').grid(row=4, column=0, sticky='ew')
-            Button(root, text='Fives').grid(row=5, column=0, sticky='ew')
-            Button(root, text='Sixes').grid(row=6, column=0, sticky='ew')
+            Button(root, text='Ones', command=place_ones).grid(row=1, column=0, sticky='ew')
+            Button(root, text='Twos', command=place_twos).grid(row=2, column=0, sticky='ew')
+            Button(root, text='Threes', command=place_threes).grid(row=3, column=0, sticky='ew')
+            Button(root, text='Fours', command=place_fours).grid(row=4, column=0, sticky='ew')
+            Button(root, text='Fives', command=place_fives).grid(row=5, column=0, sticky='ew')
+            Button(root, text='Sixes', command=place_sixes).grid(row=6, column=0, sticky='ew')
             Label(root, text='TOP SCORE', borderwidth=1, relief='raised').grid(row=7, column=0, pady=5, sticky='ew')
             Label(root, text='TOP BONUS', borderwidth=1, relief='raised').grid(row=8, column=0, pady=5, sticky='ew')
-            Button(root, text='One pair').grid(row=9, column=0, sticky='ew')
-            Button(root, text='Two pairs').grid(row=10, column=0, sticky='ew')
-            Button(root, text='Three of a kind').grid(row=11, column=0, sticky='ew')
-            Button(root, text='Four of a kind').grid(row=12, column=0, sticky='ew')
-            Button(root, text='Full house').grid(row=13, column=0, sticky='ew')
-            Button(root, text='Low straight').grid(row=14, column=0, sticky='ew')
-            Button(root, text='High straight').grid(row=15, column=0, sticky='ew')
-            Button(root, text='Chance').grid(row=16, column=0, sticky='ew')
-            Button(root, text='Yatzy').grid(row=17, column=0, sticky='ew')
+            Button(root, text='One pair', command=place_one_pair).grid(row=9, column=0, sticky='ew')
+            Button(root, text='Two pairs', command=place_two_pair).grid(row=10, column=0, sticky='ew')
+            Button(root, text='Three of a kind', command=place_three_of_kind).grid(row=11, column=0, sticky='ew')
+            Button(root, text='Four of a kind', command=place_four_of_kind).grid(row=12, column=0, sticky='ew')
+            Button(root, text='Full house', command=place_full_house).grid(row=13, column=0, sticky='ew')
+            Button(root, text='Low straight', command=place_low_straight).grid(row=14, column=0, sticky='ew')
+            Button(root, text='High straight', command=place_high_straight).grid(row=15, column=0, sticky='ew')
+            Button(root, text='Chance', command=place_chance).grid(row=16, column=0, sticky='ew')
+            Button(root, text='Yatzy', command=place_yatzy).grid(row=17, column=0, sticky='ew')
             Label(root, text='BOTTOM SCORE', borderwidth=1, relief='raised').grid(row=18, column=0, pady=5,  sticky='ew')
             Label(root, text='TOTAL SCORE', borderwidth=1, relief='raised').grid(row=19, column=0, pady=5, sticky='ew')
 
@@ -156,7 +306,7 @@ def game(player_list):
             d3_button = Button(root, text="", command= hold_dice3)
             d4_button = Button(root, text="", command= hold_dice4)
             d5_button = Button(root, text="", command= hold_dice5)
-
+            
             button_list = [d1_button, d2_button, d3_button, d4_button, d5_button]
 
             def roll():
@@ -239,10 +389,9 @@ def game(player_list):
             btn = Button(root, width=20, height=2, bg='lightyellow', text="Roll Dice", command= roll)
             btn.grid(row= 10, rowspan=3, column=10, columnspan=5)
 
-        
             root.mainloop()
 
-            print('dice numbers: ', dice_numbers)
+            print('Dice numbers: ', dice_numbers)
 
             print(root.counter)
 
@@ -289,6 +438,5 @@ def main():
     game(player_list)
 
     #winner(player_list)
-
-    
+ 
 main()
